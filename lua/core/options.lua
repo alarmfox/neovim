ea2701b5a4f09a06eab4476c3e9d371d8228d6f5
giuseppe <capassog97@gmail.com>
2024-08-18 12:20:37 +0200

    Added options for spacing, clipboard and refactoring

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,8 +1,8 @@
-vim.o.termguicolors = true
 vim.cmd("let g:netrw_liststyle = 3")
 
 local opt = vim.opt
 
+-- tabs and indentation
 opt.expandtab = true
 opt.tabstop = 2
 opt.softtabstop = 2
@@ -10,3 +10,22 @@ opt.shiftwidth = 2
 
 opt.relativenumber = true
 opt.number = true
+opt.wrap = true
+
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.cursorline = true
+
+-- termgui forr colors
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start" --allow backspace on indent, end of line and insert mode start
+
+-- clipboard
+opt.clipboard:append("unnamedplus") -- system clipboard as default registeer
