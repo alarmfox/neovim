f74bfdf2db81b0a3e23851625415f910aa3636dc
giuseppe <capassog97@gmail.com>
2024-08-18 12:14:37 +0200

    Merged settings.lua in options.lua

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,2 +1,12 @@
 vim.o.termguicolors = true
 vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+opt.expandtab = true
+opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
+
+opt.relativenumber = true
+opt.number = true
