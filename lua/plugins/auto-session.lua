8e95ba300a86eb89e5e76d20502e7a617d651693
giuseppe <capassog97@gmail.com>
2024-08-19 12:04:48 +0200

    Refactoring

--- lua/plugins/auto-session.lua
+++ lua/plugins/auto-session.lua
@@ -1,17 +1,17 @@
 return {
-  'rmagatti/auto-session',
-  lazy = false,
-  dependencies = {
-    'nvim-telescope/telescope.nvim', -- Only needed if you want to use sesssion lens
-  },
-  opts = {
-    auto_session_suppress_dirs = { '~/', '~/dev', '~/Downloads', '/' },
-    auto_restore_enabled = false,
-    auto_save_enabled = true
-  },
-  keys = {
-    { "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session based on the cwd"} },
-    { "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session based on cwd"} },
-    { "<leader>wR", "<cmd>SessionSearch<CR>", { desc = "Search session"} },
-  }
+	"rmagatti/auto-session",
+	lazy = false,
+	dependencies = {
+		"nvim-telescope/telescope.nvim", -- Only needed if you want to use sesssion lens
+	},
+	opts = {
+		auto_session_suppress_dirs = { "~/", "~/dev", "~/Downloads", "/" },
+		auto_restore_enabled = false,
+		auto_save_enabled = true,
+	},
+	keys = {
+		{ "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session based on the cwd", silent = true } },
+		{ "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session based on cwd", silent = true } },
+		{ "<leader>wR", "<cmd>SessionSearch<CR>", { desc = "Search session", silent = true } },
+	},
 }
