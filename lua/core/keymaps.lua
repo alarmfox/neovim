d4a55b1d73a46fe78b810c3b343f643b2f94ab68
giuseppe <capassog97@gmail.com>
2024-08-22 11:17:10 +0200

    Added split bindings

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,4 +3,6 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
 vim.keymap.set("n", "<leader>nh", "<cmd> nohl<CR>", { desc = "Clear search highlights" })
 
--- window management
+-- split management
+vim.keymap.set("n", "<leader>v", "<cmd>vsplit<CR>", { desc = "Vertical split" })
+vim.keymap.set("n", "<leader>s", "<cmd>split<CR>", { desc = "Horizontal split" })
