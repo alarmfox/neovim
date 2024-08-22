27caeeb457cecdfdb5a69379bebddb9a4037b4f9
giuseppe <capassog97@gmail.com>
2024-08-22 14:28:03 +0200

    Fix split command

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,6 +3,13 @@ vim.g.mapleader = " "
 vim.keymap.set("i", "jj", "<ESC>", { desc = "Exit insert mode with jj" })
 vim.keymap.set("n", "<leader>nh", "<cmd> nohl<CR>", { desc = "Clear search highlights" })
 
--- split management
-vim.keymap.set("n", "<leader>v", "<cmd>vsplit<CR>", { desc = "Vertical split" })
-vim.keymap.set("n", "<leader>s", "<cmd>split<CR>", { desc = "Horizontal split" })
+-- window management
+vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
+vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontally" })
+vim.keymap.set("n", "<leader>sx", "<cmd>close<cr>", { desc = "Close current split" })
+
+-- navigate in insert mode
+vim.keymap.set("i", "<C-h>", "<Left>", { desc = "Move left" })
+vim.keymap.set("i", "<C-j>", "<Down>", { desc = "Move down" })
+vim.keymap.set("i", "<C-k>", "<Up>", { desc = "Move up" })
+vim.keymap.set("i", "<C-l>", "<Right>", { desc = "Move right" })
